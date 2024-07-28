eba52b1d4a6995fba911eb15ad7fb1def3ed9929
George benedict <geobene13@gmail.com>
2024-07-28 10:16:14 -0700

    version 1

---
+++ script.js
@@ -0,0 +1,13 @@
+var allAthletes = []
+
+function register() {
+    // alert("Registered")
+
+    var althlete = document.getElementById("athlete").value
+    var gnder = document.getElementById("athlete").value
+    var althlete = document.getElementById("athlete").value
+    var althlete = document.getElementById("athlete").value;
+    if (althlete ==) {
+        
+    }
+}
